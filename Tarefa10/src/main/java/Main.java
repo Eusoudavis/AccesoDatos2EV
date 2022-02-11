6f426157559a42e85cc1f6b92b3ed294656f9c6c
Eusoudavid <nusubre@gmail.com>
2022-02-11 16:37:03 +0400

    Created menu

--- Tarefa10/src/main/java/Main.java
+++ Tarefa10/src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -15,8 +16,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        readById();
-    }
+        int opcion = 0;
+        do {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("1.- Crear revista");
+                System.out.println("2.- Buscar todas as revistas .");
+                System.out.println("3.- Borrar revista.");
+                System.out.println("4.- Buscar revista.");
+                System.out.println("Salir.");
+                opcion = sc.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        getRevista();
+                        break;
+                    case 2:
+                        oDeLer();
+                        break;
+                    case 3:
+                        readToDelete();
+                        break;
+                    case 4:
+                        readById();
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Opción erronea");
+                }
+            } catch (Exception e) {
+                System.out.println("La opción tiene que ser un número");
+            }
+        } while (opcion != 6);    }
 
     public static String leerDatos(final String s) throws IOException {
         System.out.println(s);
